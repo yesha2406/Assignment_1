3ce513fd6958b86809a330fa8ed37ab7e185034c
Yesha <shahyesha24111995@gmail.com>
2020-06-11 18:18:30 +0530

    V2

--- routes/product.js
+++ routes/product.js
@@ -1,75 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-var Response = require('../response');
+var productController = require('../controllers/product.controller');
 
-var productSchemaModel = require('../schema/productSchema');
 
-// API for Insert Product.
-router.post('/add-product-api', function (req, res) {
-    const productSchemaBodyData = {
-        product_name: req.body.product_name,
-        product_description: req.body.product_description,
-        product_price: req.body.product_price,
-        product_color: req.body.product_color,
-        product_size: req.body.product_size
-    }
 
-    let data = productSchemaModel(productSchemaBodyData);
+/* API for Insert Product. */
+router.post('/add-product-api', productController.newProduct);
 
-    data.save(function (err) {
-        if (err)
-            Response.errorResponse(err, res);
-        else
-            Response.successResponse('Product is added..!!', res, {});
-    });
-});
+/* API for Get all Products. */
+router.get('/get-all-products-api', productController.displayProducts);
 
-// API for Get all Products.
-router.get('/get-all-products-api', function (req, res) {
-    productSchemaModel.find(function (err, product) {
-        if (err)
-            Response.errorResponse(err, res);
-        else
-            res.json(product);
-    });
-});
+/* API for Sorting Price in Ascending order Product. */
+router.get('/sortPriceByLowtoHigh-api', productController.ascPriceProducts);
 
-// API for Sorting Price in Ascending order Product.
-router.get('/sortPriceByLowtoHigh-api', function (req, res) {
-    productSchemaModel.find()
-        .sort({ product_price: 1 })
-        .then(products => {
-            return res.json(products);
-        });
-});
+/* API for Sorting Price in Descending order Product. */
+router.get('/sortPriceByHightoLow-api', productController.desPriceProducts);
 
-// API for Sorting Price in Descending order Product.
-router.get('/sortPriceByHightoLow-api', function (req, res) {
-    productSchemaModel.find()
-        .sort({ product_price: -1 })
-        .then(products => {
-            return res.json(products);
-        });
-});
+/* API for Sorting by Name in Ascending order Product. */
+router.get('/sortName-asc-api', productController.ascNameProducts);
 
-// API for Sorting by Name in Ascending order Product.
-router.get('/sortName-asc-api', function (req, res) {
-    productSchemaModel.find()
-        .sort({ product_name: 1 })
-        .then(products => {
-            return res.json(products);
-        });
-});
+/* API for Sorting by Name in Descending order Product. */
+router.get('/sortName-des-api', productController.desNameProducts);
 
-// API for Sorting by Name in Descending order Product.
-router.get('/sortName-des-api', function (req, res) {
-    productSchemaModel.find()
-        .sort({ product_name: -1 })
-        .then(products => {
-            return res.json(products);
-        });
-});
-// API for Insert Product.
+
 
 module.exports = router;
